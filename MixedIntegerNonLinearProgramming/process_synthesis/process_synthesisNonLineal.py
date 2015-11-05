44b0911be4b2133ea88203c7a449233fd0b83419
Planelles20 <planelles20@gmail.com>
2015-11-05 22:30:49 +0100

    Ejemplo de un proceso MINLP con sus resultados

---
+++ MixedIntegerNonLinearProgramming/process_synthesis/process_synthesisNonLineal.py
@@ -0,0 +1,48 @@
+from __future__ import division
+import numpy as np
+from pyomo.environ import *
+
+model = ConcreteModel()
+
+
+model.yI   = Var(within=Binary)
+model.yII  = Var(within=Binary)
+model.yIII = Var(within=Binary)
+
+model.CosteI   = Var()
+model.CosteII  = Var()
+model.CosteIII = Var()
+
+model.AI   = Var(domain=NonNegativeReals)
+
+model.Bext   = Var(domain=NonNegativeReals)
+model.BI   = Var(domain=NonNegativeReals)
+model.BII  = Var(domain=NonNegativeReals)
+model.BIII = Var(domain=NonNegativeReals)
+
+model.C    = Var(domain=NonNegativeReals)
+model.CII  = Var(domain=NonNegativeReals)
+model.CIII = Var(domain=NonNegativeReals)
+
+#constraints
+
+model.ConstI1 = Constraint(expr=(model.CosteI == 240*model.AI+1100*model.yI))
+model.ContI2 = Constraint(expr=(model.BI == 0.9*model.AI))
+model.ContI3 = Constraint(expr=(model.AI <= 40*model.yI))
+
+model.ConstII1 = Constraint(expr=(model.CosteII == 380*model.BII+1600*model.yII))
+model.ConstII2 = Constraint(expr=(model.CII == 6.5*log(1+model.BII)))
+model.ConstII3 = Constraint(expr=(model.BII <= 40*model.yII))
+
+model.ConstIII1 = Constraint(expr=(model.CosteIII == 525*model.BIII+2100*model.yIII))
+model.ConstIII2 = Constraint(expr=(model.CIII == 7.2*log(1+model.BIII)))
+model.ConstIII3 = Constraint(expr=(model.BIII <= 40*model.yIII))
+
+model.balance_C = Constraint(expr=(model.CII+model.CIII == model.C))
+model.balance_B = Constraint(expr=(model.BI+model.Bext == model.BII+model.BIII))
+
+model.binary = Constraint(expr=(model.yII+model.yIII == 1))
+model.upA = Constraint(expr=(model.AI<=32))
+model.upC = Constraint(expr=(model.C<=20))
+
+model.Obj = Objective(expr=(1900*model.C-550*model.AI-950*model.Bext-model.CosteI-model.CosteII-model.CosteIII), sense=maximize)
